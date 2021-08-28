08de7af5077b06e46346dc686062106f7901ef9d
Sandeep Gupta <55271042+sgblkc@users.noreply.github.com>
2021-08-28 11:15:26 +0530

    Delete migrations directory

--- migrations/2_contracts_migrations.js
+++
@@ -1,7 +0,0 @@
-const ERC20Token = artifacts.require("ERC20Token");
-const SupplyChain = artifacts.require("SupplyChain");
-
-module.exports = function(deployer) {
-    deployer.deploy(ERC20Token, 10000, 18, "BUV Token", "BUV");
-    deployer.deploy(SupplyChain);
-}
